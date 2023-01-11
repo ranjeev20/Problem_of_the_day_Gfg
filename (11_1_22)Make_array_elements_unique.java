3474b4257f880da91662c8c08e9b0bc7ed42b6a9
ranjeev20 <ranjeevbohara2058@gmail.com>
2023-01-12 00:10:59 +0530

    make array elements unique

--- (11_1_22)Make_array_elements_unique.java
+++ (11_1_22)Make_array_elements_unique.java
@@ -3,11 +3,11 @@
 // User function Template for Java
 
 class Solution {
-    public long minIncrements(int[] arr, int n) {
+    public long minIncrements(int[] arr, int N) {
         // Code here
         Arrays.sort(arr);
         int count = 0;
-        for(int i=1; i<n; i++ ){
+        for(int i=1; i<N; i++ ){
             if(arr[i] <= arr[i-1]){
                 int temp = arr[i-1]-arr[i]+1;
                 arr[i] = arr[i] + temp;
